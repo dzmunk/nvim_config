ff6b7071731d79a88abfad445f64023d4bdf2aeb
dzmunk <dzmunk@yahoo.dk>
2024-11-09 15:03:26 +0100

    Update cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,19 +3,43 @@ local M = {}
 
 function M.setup()
   local cmp = require('cmp')
+  local luasnip = require('luasnip')
 
   cmp.setup({
+    snippet = {
+      -- Specify LuaSnip as the snippet engine
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
     mapping = cmp.mapping.preset.insert({
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- Accept the currently selected item
-      ['<Tab>'] = cmp.mapping.select_next_item(),
-      ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
+      { name = 'nvim_lsp' },  -- LSP completion
+      { name = 'luasnip' },   -- Snippet completion
       { name = 'buffer' },
       { name = 'path' },
-    })
+    }),
   })
 end
 
