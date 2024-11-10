6d749d08b9f8d3908df952fd6bcfcdb9be3b38cb
dzmunk <dzmunk@yahoo.dk>
2024-11-10 23:12:35 +0100

    Update nvim-tree.lua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -10,9 +10,18 @@ function M.setup()
     sync_root_with_cwd = true,
     disable_netrw = true,
     hijack_cursor = true,
+     renderer = {
+      root_folder_label = false,
+    },
+    update_cwd = false,
     update_focused_file = {
       enable = true,
       update_root = false,
+    },
+    actions = {
+      change_dir = {
+        enable = false,
+      },
     },
   })
 end
