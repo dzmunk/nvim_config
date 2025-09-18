a0b4b9e57563d99858df27273a5a40d8b3647523
dzmunk <dzmunk@yahoo.dk>
2025-09-18 22:00:22 +0200

    Create install_neovim.sh

---
+++ install_neovim.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DIR="$HOME/neovim"
+
+if [ "${1:-}" = "--uninstall" ]; then
+  cd "$DIR"
+  sudo make uninstall
+  exit 0
+fi
+
+if [ -d "$DIR/.git" ]; then
+  git -C "$DIR" fetch --tags --force
+else
+  mkdir -p "$(dirname "$DIR")"
+  git clone https://github.com/neovim/neovim.git "$DIR"
+fi
+
+git -C "$DIR" -c advice.detachedHead=false checkout -f nightly
+git -C "$DIR" reset --hard nightly
+
+cd "$DIR"
+make distclean || true
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+
+nvim --version | head -n 3
