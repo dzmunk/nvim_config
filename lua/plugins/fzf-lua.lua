adadc8dc50fe0ef87f80345c6686301e06ff91be
dzmunk <dzmunk@yahoo.dk>
2025-10-15 12:23:17 +0200

    Update fzf-lua.lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -39,6 +39,11 @@ return {
                         ["default"] = actions.grep_lgrep,
                     },
                 },
+                winopts = {
+                    on_create = function()
+                        vim.keymap.set("t", "<C-r>", [['<C-\><C-N>"'.nr2char(getchar()).'pi']], { expr = true, buffer = true })
+                    end
+                },
             }
         end,
     },
