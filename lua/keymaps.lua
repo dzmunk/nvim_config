64ec8e66313ca7f715cc269620a27014720dea3f
dzmunk <dzmunk@yahoo.dk>
2025-12-05 23:22:41 +0100

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,16 +1,13 @@
--- Space as leader
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 local map = vim.keymap.set
 
--- Window navigation
 map('n', '<C-h>', '<C-w>h', { remap = true })
 map('n', '<C-j>', '<C-w>j', { remap = true })
 map('n', '<C-k>', '<C-w>k', { remap = true })
 map('n', '<C-l>', '<C-w>l', { remap = true })
 
--- Pop up menu navigation
 map('i', '<Tab>', function()
     return vim.fn.pumvisible() == 1 and '<Down>' or '<Tab>'
 end, { expr = true, desc = 'Next completion or tab' })
@@ -26,7 +23,6 @@ map('i', '<C-Space>', function()
     return '<C-n>'
 end, { expr = true, desc = 'Trigger completion menu' })
 
--- Smart escape
 map({ 'i', 'v', 'n' }, '<esc>', function()
     if vim.v.hlsearch == 1 then
         vim.cmd('noh')
