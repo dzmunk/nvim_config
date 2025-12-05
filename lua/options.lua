b3ebced1cc1c01f88da46fdc529f1794550dfbe4
dzmunk <dzmunk@yahoo.dk>
2025-12-05 21:23:21 +0100

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -33,8 +33,8 @@ o.grepformat = "%f:%l:%c:%m"
 o.grepprg    = "rg --vimgrep --smart-case"
 
 o.autocomplete        = false
-o.autocompletedelay   = 120
-o.autocompletetimeout = 120
+-- o.autocompletedelay   = 120
+-- o.autocompletetimeout = 120
 o.completeopt         = "popup,menu,menuone,noinsert,fuzzy"
 o.complete            = ".,w^5,b^5,o^5"
 o.pumheight           = 5
